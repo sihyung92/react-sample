fafb1f7260384f4b06b612b05b9ed8c27df0fd95
SiHyung92 <fjzjqhdl@gmail.com>
2020-08-13 11:27:59 +0900

    feat : 클릭 시 X로 바뀌는 기능 구현

--- src/index.js
+++ src/index.js
@@ -2,19 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+class Square extends React.Component {     
+    constructor(props){
+        super(props);
+        this.state = {
+            value : null,
+        }
+    }
+    render() {
+        return (
+        <button
+         className="square"
+         onClick={() => { this.setState({value : 'X'}); }}
+        >
+            {this.state.value}
+        </button>
+        );
+    }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value = {i} />;
   }
 
   render() {
