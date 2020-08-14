db5e9d0b4f25bde058df6427583992a38b5ad442
SiHyung92 <fjzjqhdl@gmail.com>
2020-08-14 15:52:28 +0900

    feat : history 추가 및 시간이동 버튼 추가

--- src/index.js
+++ src/index.js
@@ -77,7 +77,13 @@ class Game extends React.Component {
     
     const moves = history.map((step,move) => {
       const desc = move ?
-
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
     })
 
     let status;
@@ -98,7 +104,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
